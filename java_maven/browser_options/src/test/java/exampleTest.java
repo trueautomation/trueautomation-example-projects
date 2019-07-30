4790194fede8e15f86f5053886ef5ce8f89df2aa
Grigoriy Stetsenko <stetsenko@softesis.com>
2019-07-30 16:29:10 +0300

    adding example project for browser options on java

---
+++ java_maven/browser_options/src/test/java/exampleTest.java
@@ -0,0 +1,41 @@
+import io.trueautomation.client.driver.TrueAutomationDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.trueautomation.client.TrueAutomationHelper.ta;
+
+public class exampleTest {
+    private WebDriver driver;
+    private By loginBtn = By.xpath(ta("loginBtn", "//a[contains(., 'Login')]"));
+    private By signupBtn = By.cssSelector(ta("signupBtn", "div.sign-up-container > a"));
+    private By emailFl = By.name(ta("emailFl", "email"));
+
+    @BeforeTest
+    public void beforeTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        driver = new TrueAutomationDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void exampleTest() {
+        driver.get("https://trueautomation.io");
+
+        driver.findElement(loginBtn).click();
+        driver.findElement(signupBtn).click();
+        driver.findElement(emailFl).sendKeys("your@mail.com");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
