3b1ee7953fdd5796118e7c5ab8b1b1f2e726c68e
Grigoriy Stetsenko <stetsenko@softesis.com>
2019-09-18 12:26:29 +0300

    some edits of sync_elements test

--- capybara_rspec/sync_elements/spec/test_scenario/test.rb
+++ capybara_rspec/sync_elements/spec/test_scenario/test.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 feature 'TrueAutomation.IO capybara example' do
-  scenario 'Test example' do
-    visit 'https://app.trueautomation.io'
 
-    1000.times do |i|
+  1000.times do |i|
+    scenario 'Test example' do
+      visit 'https://app.trueautomation.io'
+
       find(:css, ta("trueautomation:sign_UP#{i + 1}", 'div.sign-up-container > a')).click
       sleep 1
       find(:xpath, ta("trueautomation:sign_IN#{i + 1}", "//a[text()='Sign in']")).click
