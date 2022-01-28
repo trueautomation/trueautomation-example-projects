c51b59980182f96d8304137fb79252d1f841f79c
Grigoriy Stetsenko <stetsenko@softesis.com>
2022-01-28 22:10:29 +0200

    fix start url

--- capybara_rspec/remote_webdriver/spec/test_scenario/example_test.rb
+++ capybara_rspec/remote_webdriver/spec/test_scenario/example_test.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 feature 'TrueAutomation.IO Capybara example' do
   scenario 'Test example' do
-    visit 'https://app.trueautomation.io/'
+    visit 'https://trueautomation.io/'
 
     find(:xpath, ta('homePage:loginBtn', "//a[.//span[text()='Login']]")).click
     find(:css, ta('signinPage:signupBtn', 'div.sign-up-container > a')).click
